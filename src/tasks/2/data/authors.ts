ce6449c3b76556ce67c261be0294511fa5ad2047
Vladimir Breusov <v.breusov@kefirgames.com>
2021-07-05 19:33:48 +0300

    feat: quotes

---
+++ src/tasks/2/data/authors.ts
@@ -0,0 +1,59 @@
+import img1 from "src/assets/obi-wan.0.jpg";
+import img2 from "src/assets/yoda.jpeg";
+import img3 from "src/assets/veider.jpeg";
+import img4 from "src/assets/walker.jpeg";
+import img5 from "src/assets/luke.jpeg";
+import img6 from "src/assets/han-solo.jpg";
+import img7 from "src/assets/gon.jpeg";
+import img8 from "src/assets/palpatine.jpeg";
+import img9 from "src/assets/general.jpeg";
+
+const authors: any[] = [
+    {
+        id: 1,
+        name: "Obi-Wan Kenobi",
+        avatar: img1,
+    },
+    {
+        id: 2,
+        name: "Yoda",
+        avatar: img2,
+    },
+    {
+        id: 3,
+        name: "Darth Vader",
+        avatar: img3,
+    },
+    {
+        id: 4,
+        name: "Anakin Skywalker",
+        avatar: img4,
+    },
+    {
+        id: 5,
+        name: "Luke Skywalker",
+        avatar: img5,
+    },
+    {
+        id: 6,
+        name: "Han Solo",
+        avatar: img6,
+    },
+    {
+        id: 7,
+        name: "Qui-Gon Jinn",
+        avatar: img7,
+    },
+    {
+        id: 8,
+        name: "Palpatine",
+        avatar: img8,
+    },
+    {
+        id: 9,
+        name: "General Grievous",
+        avatar: img9,
+    },
+];
+
+export default authors;
