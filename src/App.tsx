1e5b8bde6aa7197d5f8bed1de6246cf8d13293c3
Vladimir Breusov <v.breusov@kefirgames.com>
2021-07-05 22:09:35 +0300

    feat: comments list init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Stopwatch from "./tasks/1/Stopwatch";
+import CommentsList from "src/tasks/2/CommentsList";
 
 function App() {
     return (
         <div className="App">
-            <Stopwatch />
+            <CommentsList />
         </div>
     );
 }
